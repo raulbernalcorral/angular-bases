5d27f70e558e6f2c77fb293813baed37c3cace09
RAUL BERNAL <RAUL_BERNAL@jabil.com>
2023-06-14 15:32:51 -0600

    primer commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }];
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  // onDeleteCharacter(index:number):void{
+
+  //   console.log(index);
+
+  //   this.onDelete.emit(index);
+  // }
+
+  deleteCharacterById(id?:string):void{
+
+    if(!id) return;
+
+    this.onDelete.emit(id);
+  }
+
+}
